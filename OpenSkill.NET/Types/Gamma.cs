0940f8e5b314911fb9bff4f37ab50255c1ef3a57
JustRoxy <JustRoxyOsu@inbox.ru>
2023-03-07 19:39:43 +0300

    refactor: suppress null warnings

--- OpenSkill.NET/Types/Gamma.cs
+++ OpenSkill.NET/Types/Gamma.cs
@@ -6,6 +6,6 @@ public class Gamma
     public double K { get; set; }
     public double Mu { get; set; }
     public double SigmaSq { get; set; }
-    public Team Team { get; set; }
+    public Team Team { get; set; } = null!;
     public double QRank { get; set; }
 }
--- OpenSkill.NET/Types/TeamRating.cs
+++ OpenSkill.NET/Types/TeamRating.cs
@@ -4,6 +4,6 @@ public class TeamRating
 {
     public double Mu { get; set; }
     public double SigmaSq { get; set; }
-    public Team Team { get; set; }
+    public Team Team { get; set; } = null!;
     public double Rank { get; set; }
 }
